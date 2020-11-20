6bf476e2f7b34c17d67528cee3a2f74548ec0f10
碧海青天 <74009293+bh-qt@users.noreply.github.com>
2020-11-20 23:54:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let readSync = util.readSync
 let writeSync = util.writeSync
 
 let BUILD_DIR = 'dist' // 构建目录
-let ENTRY_FILE = '//suc.bihai.ml/sub?target=v2ray&url=https://proxypoolss.tk/clash/proxies?c=HK,TW,JP,SG,CN&list=true&filename=node.txt' // SS(R)入口
+let ENTRY_FILE = '//suc.bihai.ml/sub?target=ssr&url=https://proxypoolss.tk/clash/proxies?c=HK,TW,JP,SG,CN&list=true&filename=node.txt' // SS(R)入口
 // let ENTRY_FILE = './node.txt'
 
 let str = readSync(ENTRY_FILE)
