f0b630e9a37c952d749ee1ff1ca6fa665fe0b205
碧海青天 <74009293+bh-qt@users.noreply.github.com>
2021-02-10 01:03:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,18 +7,15 @@
  */
  
 let path = require('path')
-//let URL = require('URL') 
 let util = require('./util')
 let base64 = util.base64
 let readSync = util.readSync
 let writeSync = util.writeSync
 
 let BUILD_DIR = 'dist' // 构建目录
-// let ENTRY_FILE = './node.txt'
-let URL = 'https://gh.188ck.cn/ss/ssdy.txt'
+let ENTRY_FILE = './node.txt'
 
-let str = readSync(URL)
-//let str = readSync(ENTRY_FILE)
+let str = readSync(ENTRY_FILE)
 
 let checker = item => {
     return item => item.includes('ssr://') || item.includes('ss"//')
